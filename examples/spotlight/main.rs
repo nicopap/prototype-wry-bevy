328ef1e30da5d187718cd126013df4371d3010c1
Nicola Papale <nico@nicopap.ch>
2023-09-07 13:28:06 +0200

    Initial commit
    This is basically a copy/paste of bevy_winit with an additional example
    and overwritting of the winit dependency to use wusyong's winit fork

---
+++ examples/spotlight/main.rs
@@ -0,0 +1,91 @@
+use bevy::prelude::*;
+use bevy_winit_gtk;
+
+fn main() {
+    App::new()
+        .add_plugins((
+            DefaultPlugins
+                .set(bevy::log::LogPlugin {
+                    level: bevy::log::Level::INFO,
+                    filter: "wgpu_core=warn".to_string(),
+                })
+                .disable::<bevy::winit::WinitPlugin>(),
+            bevy_winit_gtk::WinitPlugin,
+        ))
+        .insert_resource(ClearColor(Color::rgb(0., 0.1, 0.2)))
+        .add_systems(Startup, setup)
+        .add_systems(Update, (movement, animate_light_direction))
+        .run();
+}
+
+#[derive(Component)]
+struct Movable;
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(5.0).into()),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+    // cube
+    commands.spawn((
+        Movable,
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            ..default()
+        },
+    ));
+    // light
+    commands.spawn(SpotLightBundle {
+        spot_light: SpotLight {
+            intensity: 3500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(2.0, 4.0, 2.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+fn animate_light_direction(time: Res<Time>, mut query: Query<&mut Transform, With<SpotLight>>) {
+    for mut transform in &mut query {
+        transform.rotate_y(time.delta_seconds() * 0.5);
+    }
+}
+
+fn movement(
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+    mut query: Query<&mut Transform, With<Movable>>,
+) {
+    for mut transform in &mut query {
+        let mut direction = Vec3::ZERO;
+        if input.pressed(KeyCode::Up) {
+            direction.y += 1.0;
+        }
+        if input.pressed(KeyCode::Down) {
+            direction.y -= 1.0;
+        }
+        if input.pressed(KeyCode::Left) {
+            direction.x -= 1.0;
+        }
+        if input.pressed(KeyCode::Right) {
+            direction.x += 1.0;
+        }
+
+        transform.translation += time.delta_seconds() * 2.0 * direction;
+    }
+}
